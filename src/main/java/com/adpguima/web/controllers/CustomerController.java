c6f1eeb54a228b6e27fc9d2713ed703ac03602e5
Andre Guimaraes <andrepaivaguimaraes@gmail.com>
2019-01-29 20:54:08 -0200

    Add xml configuration and first controller

---
+++ src/main/java/com/adpguima/web/controllers/CustomerController.java
@@ -0,0 +1,22 @@
+package com.adpguima.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CustomerController {
+
+	
+	@ResponseBody
+	@RequestMapping("/")
+	public String home() {		
+		return "Welcome home!";
+	}
+	
+	@RequestMapping("/welcome")
+	public String welcome() {
+		
+		return "welcome";
+	}
+}
